fa536560b0a2508ebdd0e59c0e2e7ffc0f35035f
Kris <kristian_dilov@abv.bg>
2022-12-14 21:30:05 +0000

    initial commit

---
+++ src/example/hello.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello World")
+	var x int = 5
+	var z int = 14
+	fmt.Println(x+z)
+	k := 18
+	fmt.Println(k)
+
+	if x>12 {
+		fmt.Println("Big number")
+	}
+
+}
