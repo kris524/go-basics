5e21f16c96458cfda22ea6348b5b348328801ddb
Kris <kristian_dilov@abv.bg>
2022-12-23 18:40:55 +0000

    go dictionary

--- src/example/hello.go
+++ src/example/hello.go
@@ -21,7 +21,23 @@ func main() {
 	fmt.Println(a)
 
 	b := [4]int{4,9,8,3}
+	c := []int{4,9,8,3}
+
+	c = append(c, 65)
+
 
 	fmt.Println(b)
+	fmt.Println(c)
+	mapper()
 
+
+}
+
+func mapper() {
+	mapper := make(map[string]int)
+	mapper["a"] = 1
+	mapper["b"] = 2
+	mapper["c"] = 3
+	
+	fmt.Println(mapper)
 }
